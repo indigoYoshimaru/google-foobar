85056d43d5bef755f7df793a5bb5f7c466a4ffeb
indigoYoshimaru <linh.phung1@onemount.com>
2024-02-11 02:36:33 +0700

    finish level 4

--- level4/distract-the-trainers/solution.py
+++ level4/distract-the-trainers/solution.py
@@ -1,82 +1,60 @@
-import math
-def is_pair(min_val, max_val, sum_val, min_list): 
-    # print(f'{min_val=}-{max_val=}-{sum_val=}-{min_list}')
-    if sum_val%2!=0: 
-        return True
-    if min_val in min_list: 
-        return True
-    mid_val = sum_val/2
-    if min_val == mid_val: 
-        return False
+# my terrible mistake
+# def is_pair(min_val, max_val, val_list): 
+#     print(f'{min_val=}-{max_val=}-{val_list}')
+#     if (min_val+max_val)%2: 
+#         return True
+#     if min_val in val_list: 
+#         return True
+#     mid_val = (min_val+max_val)/2
+#     if min_val == mid_val: 
+#         return False
     
-    min_list.append(min_val)
-    k = math.ceil(math.log2((max_val-mid_val)/float(min_val)+1))+1
-    new_vals = [
-        max_val - (2**(k-1)-1)*min_val,
-        min_val + (2**(k-1)-1)*min_val,
-    ]
-    min_val = min(new_vals)
-    max_val = max(new_vals)
-    # print(f'{k=}-{new_vals=}')
-    return is_pair(min_val, max_val, sum(new_vals), min_list)
+#     val_list.append(min_val)
+#     k = max(math.ceil(math.log((max_val-mid_val)/float(min_val)+1))+1,1)
+#     new_vals = [
+#         max_val - (2**(k-1)-1)*min_val,
+#         min_val + (2**(k-1)-1)*min_val,
+#     ]
+#     min_val = min(new_vals)
+#     max_val = max(new_vals)
+#     # print(f'{k=}-{new_vals=}')
+#     return is_pair(min_val, max_val, val_list)
+ 
 
-# def to_str(i, j):
-#     return f'[{i},{j}]'
+def is_pair(x, y):
+    n_tilde = x+y
+    while n_tilde % 2 == 0:
+        n_tilde /= 2
+    return (x % n_tilde) != 0
 
-# def find_sum(trainer_pair): 
-    # visited = dict()
-    # best_sum = 0
-    # cur_sum = 0
-    # for pair, is_pair in trainer_pair.items(): 
-    #     # if not is_pair: 
-    #     #     continue
-    #     u, v = eval(pair)
-    #     if u not in visited and v not in visited: 
-    #         visited.append(u)
-    #         visited.append(v)
-    #         print(u, v)
-    #         cur_sum +=is_pair
-    #         best_sum = max(best_sum, cur_sum)
-    #         print(f"{best_sum=}-{cur_sum=}")
-    #     else: 
-    #         cur_sum +=is_pair
-    #         best_sum = max(best_sum, cur_sum)
-    #         print(f'in visited: {u}, {v}')
-    #         print(f"{best_sum=}-{cur_sum=}")
-    #     # print(visited)
 
-    # for pair, is_pair in trainer_pair.items(): 
-    #     if not is_pair: 
-    #         continue
-    #     u, v = eval(pair)
-    #     if u not in visited: 
-    #         visited[u] = [v]
-    #         print(visited) 
-    #     else:  
-    #         visited[u].append(v) 
+def bpm(banana_list, u, bunny_match, visited): 
+    for v in range(len(banana_list)):
+        if is_pair(banana_list[u],banana_list[v]) and not visited[v]:
                 
-    # print(visited)
-    # visited = dict((k, v) for k, v in visited.items() if v)
-    # print(visited)
-        
+            visited[v] = True
+            if bunny_match[v] == -1 or bpm(banana_list, bunny_match[v], 
+                                            bunny_match, visited):
+                bunny_match[v] = u
+                return True
+            
+    return False
 
 def solution(banana_list):
-    if len(banana_list)<1 or len(banana_list)>100:
+    if len(banana_list)<=1 or len(banana_list)>100:
         return len(banana_list)
-    trainer_pair = dict()
-    visited = []
-    for i, trainer in enumerate(banana_list): 
-        for j in range(i+1, len(banana_list)): 
-            val = int(is_pair(min(trainer, banana_list[j]), max(trainer, banana_list[j]), trainer+ banana_list[j], []))
-            if val: 
-                # trainer_pair[to_str(trainer, banana_list[j])] = val
-                visited.append(trainer)
-                visited.append(banana_list[j])
-                # trainer_pair[to_str(banana_list[j], trainer)] = val
-    res = list(filter(lambda x: x not in visited, banana_list))
-    print(visited)
-    # find_sum(trainer_pair)
-    return len(res)
+    banana_list = sorted(banana_list)
+    if banana_list[-1]>2**30:
+        return len(banana_list)
+
+    bunny_match = [-1] * len(banana_list)
+    result = 0
+    for trainer_i in range(len(banana_list)):
+        visited = [False] * len(banana_list)  
+        if bpm(banana_list, trainer_i, bunny_match, visited):
+            result += 1
+    return len(banana_list) - 2*(result/2)
+
 
 def run_with_test_case(func, stop_when_fail: bool = True):
     from colorama import Fore
@@ -86,15 +64,24 @@ def run_with_test_case(func, stop_when_fail: bool = True):
 
     test_cases = [
        [1, 1], 
-    #    [1,4,3,5],
+       [1,4,3,5],
        [1, 7, 3, 21, 13, 19],
+       [1,1,1,2],
+       [1,7,3,3],
+       [3,7,3,7],
+       [3,7,3,7,3],
+       [1]
     ]
 
     results = [
         2,
-        # 1,
         0,
-
+        0,
+        2, 
+        2,
+        0,
+        1,
+        1,
     ]
 
     try:
@@ -115,6 +102,5 @@ def run_with_test_case(func, stop_when_fail: bool = True):
         raise e
     else:
         print(f"{Fore.GREEN}Passed all test cases")
-
 
 run_with_test_case(solution)
